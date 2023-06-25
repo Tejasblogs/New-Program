bd78802ce14216c671cdced4ad890748b7763f1e
Tejas Tupe <93927655+Tejasblogs@users.noreply.github.com>
2023-06-25 21:08:01 +0530

    Add files via upload

---
+++ Arrays1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Arrays1 {
+    public static void main (String [] args) {
+        //int ar[];
+        int ar[] = new int [4];
+        ar=new int [7];
+
+        System.out.println(ar[6]);
+
+       
+    
+        
+    
+    }
+}
